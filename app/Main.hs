b5f497f98603516f9ea291145a8c1f2878045c72
Wybitul Eugen <wybitul.evzen@gmail.com>
2017-06-22 21:13:40 +0200

    Minor changes

--- app/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import qualified CombinatoricsKata as C
-import qualified StringKata as S
-import qualified HaskellKata as H
-import qualified NumberKata as N
-
-main :: IO ()
-main = print "Hey! Seems you've found a dead end!"
